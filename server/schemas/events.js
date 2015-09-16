3628a1c0b6e9c3209f69d8d0be6945d24aafa950
Andela Fellow <Andela8@192.168.22.107>
2015-09-16 10:25:16 +0300

    [Delivers #103477442] Refactored code indentation to tow spaces

--- server/schemas/events.js
+++ server/schemas/events.js
@@ -1,64 +1,64 @@
 //var require the seqalize module
 var Seq = require('sequelize'),
-    db = require('../config/db-connect'),
-    users = require('./users'),
-    events = db.define('events', {
-            // FOREIGN KEY
-            // references the user id in users table
-            user_id: {
-                type: Seq.INTEGER,
-                allowNull: false,
-                references: {
-                    model: users,
-                    key: 'id'
-                }
-            },
+  db = require('../config/db-connect'),
+  users = require('./users'),
+  events = db.define('events', {
+      // FOREIGN KEY
+      // references the user id in users table
+      user_id: {
+        type: Seq.INTEGER,
+        allowNull: false,
+        references: {
+          model: users,
+          key: 'id'
+        }
+      },
 
-            // event name
-            // hold the name of the events
-            ev_name: {
-                type: Seq.STRING,
-                allowNull: false,
-            },
+      // event name
+      // hold the name of the events
+      ev_name: {
+        type: Seq.STRING,
+        allowNull: false,
+      },
 
-            // description
-            description: {
-                type: Seq.TEXT,
-                allowNull: true
-            },
+      // description
+      description: {
+        type: Seq.TEXT,
+        allowNull: true
+      },
 
-            // location of the event
-            location: {
-                type: Seq.TEXT,
-                allowNull: false
-            },
+      // location of the event
+      location: {
+        type: Seq.TEXT,
+        allowNull: false
+      },
 
-            // venue of the event
-            venue: {
-                type: Seq.TEXT,
-                allowNull: false
-            },
+      // venue of the event
+      venue: {
+        type: Seq.TEXT,
+        allowNull: false
+      },
 
-            // time 
-            time: {
-                type: Seq.DATE,
-                allowNull: false
-            },
+      // time
+      time: {
+        type: Seq.DATE,
+        allowNull: false
+      },
 
-            // event sponsors
-            sponsor: {
-                type: Seq.TEXT,
-                allowNull: true
-            }
+      // event sponsors
+      sponsor: {
+        type: Seq.TEXT,
+        allowNull: true
+      }
 
-        },
-        // table configuration
-        {
-            // prevent time stamps from using camelase
-            // updatedAt to updated_at and createdAt to created-at
-            underscore: true,
-            // prevent sequelize from transforming the user tables to prural
-            freezetableName: true
-        });
+    },
+    // table configuration
+    {
+      // prevent time stamps from using camelase
+      // updatedAt to updated_at and createdAt to created-at
+      underscore: true,
+      // prevent sequelize from transforming the user tables to prural
+      freezetableName: true
+    });
 
 module.exports = events;
--- server/schemas/items-group.js
+++ server/schemas/items-group.js
@@ -1,61 +1,61 @@
 //var require the sequelize module
 var Seq = require('sequelize'),
-    db = require('../config/db-connect'),
-    users = require('./users'),
-    items = require('./items'),
-    item_group = db.define('item_group', {
+  db = require('../config/db-connect'),
+  users = require('./users'),
+  items = require('./items'),
+  item_group = db.define('item_group', {
 
-            // FOREIGN KEY
-            // references the item id in the items table
-            item_id: {
-                type: Seq.INTEGER,
-                allowNull: false,
-                references: {
-                    model: items,
-                    key: 'id'
-                }
-            },
+      // FOREIGN KEY
+      // references the item id in the items table
+      item_id: {
+        type: Seq.INTEGER,
+        allowNull: false,
+        references: {
+          model: items,
+          key: 'id'
+        }
+      },
 
-            // FOREIGN KEY
-            // references the user id in users table
-            // The id of the person sharing, sponsoring or recommending the item
-            created_by: {
-                type: Seq.INTEGER.UNSIGNED,
-                allowNull: false,
-                references: {
-                    model: users,
-                    key: 'id'
-                }
-            },
+      // FOREIGN KEY
+      // references the user id in users table
+      // The id of the person sharing, sponsoring or recommending the item
+      created_by: {
+        type: Seq.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: users,
+          key: 'id'
+        }
+      },
 
-            // FOREIGN KEY
-            // references the user id in users table
-            // The id of the person targeted
+      // FOREIGN KEY
+      // references the user id in users table
+      // The id of the person targeted
 
-            created_for: {
-                type: Seq.INTEGER.UNSIGNED,
-                allowNull: false,
-                references: {
-                    model: users,
-                    key: 'id'
-                }
-            }
-            // item name
-            // e.g sponsored, shared, recommended
-            group: {
-                type: Seq.ENUM,
-                values: ['sponsored', 'shared', 'recommended'],
-                allowNull: false,
-            }
-        },
+      created_for: {
+        type: Seq.INTEGER.UNSIGNED,
+        allowNull: false,
+        references: {
+          model: users,
+          key: 'id'
+        }
+      }
+      // item name
+      // e.g sponsored, shared, recommended
+      group: {
+        type: Seq.ENUM,
+        values: ['sponsored', 'shared', 'recommended'],
+        allowNull: false,
+      }
+    },
 
-        // table configuration
-        {
-            // prevent time stamps from using camelase
-            // updatedAt to updated_at and createdAt to created-at
-            underscore: true,
-            // prevent sequelize from transforming the user tables to prural
-            freezetableName: true
-        });
+    // table configuration
+    {
+      // prevent time stamps from using camelase
+      // updatedAt to updated_at and createdAt to created-at
+      underscore: true,
+      // prevent sequelize from transforming the user tables to prural
+      freezetableName: true
+    });
 
 module.exports = item_group;
