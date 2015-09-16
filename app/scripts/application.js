32dfe06cb9928204574770a87b5566ad1210c540
Eugene Mutai <eugenemutai@gmail.com>
2015-09-16 17:52:05 +0300

    [Chore] Converting current UI to Angular Materialize

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,35 +1,59 @@
-angular.module('vvida.controllers', []);
-angular.module('vvida.services', []);
-angular.module('vvida.filters', []);
-angular.module('vvida.directives', []);
+(function() {
+  'use strict';
+  angular.module('vvida.controllers', []);
+  angular.module('vvida.services', []);
+  angular.module('vvida.filters', []);
+  angular.module('vvida.directives', []);
 
-require('./controllers/home');
+  require('./controllers/home');
 
-window.app = angular.module('vvida', [
-  'vvida.controllers',
-  'vvida.services',
-  'vvida.filters',
-  'vvida.directives',
-  'ui.router'
-]);
+  window.app = angular.module('vvida', [
+    'vvida.controllers',
+    'vvida.services',
+    'vvida.filters',
+    'vvida.directives',
+    'ui.router',
+    'ngMaterial'
+  ]);
 
+  window.app.run(['$rootScope', function($rootScope) {
 
-window.app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
-  //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/404");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('home', {
-      url: "/",
-      templateUrl: "views/home.html"
-    })
-    .state('404', {
-      url: "/404",
-      templateUrl: "views/404.html",
-      controller: function($scope) {}
-    });
+    $rootScope.menu = [{
+      name: 'Home',
+      state: 'home'
+    }, {
+      name: 'About',
+      state: 'about'
+    }];
 
-  $locationProvider.html5Mode(true);
-}]);
+  }]);
+
+  window.app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$mdThemingProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/404");
+    //
+    // Now set up the states
+    $mdThemingProvider.theme('default')
+      .primaryPalette('blue')
+      .accentPalette('deep-orange');
+    $stateProvider
+      .state('home', {
+        url: "/",
+        controller: 'HomeCtrl',
+        templateUrl: "views/home.html"
+      })
+      .state('about', {
+        url: "/about",
+        controller: 'HomeCtrl',
+        templateUrl: "views/about.html"
+      })
+      .state('404', {
+        url: "/404",
+        templateUrl: "views/404.html",
+        controller: function($scope) {}
+      });
+
+    $locationProvider.html5Mode(true);
+  }]);
+})();
