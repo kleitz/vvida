e124e9e879d0e1f543b5cd78131ecb098394d3a0
andela-thomas <thomas.nyambati@andela.com>
2015-09-21 16:23:38 +0300

    [start #103254376] added passport and expess-session modules to the app

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express'),
-    path = require('path'),
-    env = process.env.NODE_ENV || 'development',
-    config,
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    routes = require('./server/routes'),
-    app = express();
+  path = require('path'),
+  env = process.env.NODE_ENV || 'development',
+  config = require('./server/config')[env],
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  routes = require('./server/routes'),
+  app = express(),
+  passport = require('passport'),
+  session = require('express-session');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'server/views'));
@@ -18,18 +20,28 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
+app.use(passport.initialize());
+app.use(session({
+  secret: config.expressSessionKey,
+  // store: sessionStore, // connect-mongo session store
+  proxy: true,
+  resave: true,
+  saveUninitialized: true
+}));
 
-routes(app, config);
+app.use(passport.session());
+routes(app, config, passport);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -37,27 +49,27 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (env === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
-    console.log('Express server listening on %d, in %s mode', server.address().port, app.get('env'));
+  console.log('Express server listening on %d, in %s mode', server.address().port, app.get('env'));
 });
 
 module.exports = app;
