fd80a5704f6757e1cd36d9f9169b37139646884b
Hannah Koske <hannah.koske@andela.com>
2015-09-21 13:13:24 +0300

    [Fixes #103254368] Fixed footer

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -32,6 +32,67 @@
       state: 'login'
     }];
 
+    $rootScope.discover = [{
+      name: 'The Weekly Vvida',
+      state: 'footer'
+    }, {
+      name: 'Vvida Blog',
+      state: 'footer'
+    }, {
+      name: 'Support',
+      state: 'footer'
+    }, {
+      name: 'Vvida Mobile',
+      state: 'footer'
+    }, {
+      name: 'Developers',
+      state: 'footer'
+    }, {
+      name: 'RSS Feed',
+      state: 'footer'
+    }];
+
+    $rootScope.business = [{
+      name: 'Claim your Business Page',
+      state: 'footer'
+    }, {
+      name: 'Advertise on Vvida',
+      state: 'footer'
+    }, {
+      name: 'Support',
+      state: 'footer'
+    }, {
+      name: 'Business Success Stories',
+      state: 'footer'
+    }, {
+      name: 'Business Support',
+      state: 'footer'
+    }, {
+      name: 'Vvida Blog for Business Owners',
+      state: 'footer'
+    }];
+
+    $rootScope.about = [{
+      name: 'About Vvida',
+      state: 'footer'
+    }, {
+      name: 'Press',
+      state: 'footer'
+    }, {
+      name: 'Content Guidelines',
+      state: 'footer'
+    }, {
+      name: 'Terms of Service',
+      state: 'footer'
+    }, {
+      name: 'Private Policy',
+      state: 'footer'
+    }, {
+      name: 'Ad Choices',
+      state: 'footer'
+    }];
+
+
   }]);
 
   window.app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$mdThemingProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
@@ -58,6 +119,11 @@
         url: "/events",
         controller: 'HomeCtrl',
         templateUrl: "views/events.html"
+      })
+      .state('footer', {
+        url: "/footer",
+        controller: 'HomeCtrl',
+        templateUrl: "views/footer.html"
       })
       .state('404', {
         url: "/404",
