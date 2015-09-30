9bdd7ea273fcd546b65fc4e0a9465eeb1fe45e00
andela-thomas <thomas.nyambati@andela.com>
2015-09-30 11:08:50 +0300

    [Finishes #104431664] adding heroku postgres database config vars

--- server/config/index.js
+++ server/config/index.js
@@ -1,14 +1,30 @@
 var envVariables = {
-    expressSessionKey: process.env.EXPRESS_SESSION_KEY || '436b2efb-0302-4113-9954-f658f554ea87'
+    expressSessionKey: process.env.EXPRESS_SESSION_KEY || '436b2efb-0302-4113-9954-f658f554ea87',
+    database: process.env.DATABASE_NAME ||'vvida',
+    host: process.env.HOST || 'localhost',
+    userName: process.env.USER_NAME || 'postgres',
+    password: process.env.PASSWORD || 'root'
   },
   development = {
-    expressSessionKey: envVariables.expressSessionKey
+    expressSessionKey: envVariables.expressSessionKey,
+    database: envVariables.database,
+    host: envVariables.host,
+    username: envVariables.userName,
+    password: envVariables.password,
   },
   staging = {
-    expressSessionKey: envVariables.expressSessionKey
+    expressSessionKey: envVariables.expressSessionKey,
+    database: envVariables.database,
+    host: envVariables.host,
+    username: envVariables.userName,
+    password: envVariables.password,
   },
   production = {
-    expressSessionKey: envVariables.expressSessionKey
+    expressSessionKey: envVariables.expressSessionKey,
+    database: envVariables.database,
+    host: envVariables.host,
+    username: envVariables.userName,
+    password: envVariables.password,
   };
 
 module.exports = {
