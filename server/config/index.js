e5aeca8afe2ff3948fcb6ff8a9708b1628608877
andela-thomas <thomas.nyambati@andela.com>
2015-10-02 15:01:37 +0300

    [ Finishes #104714660] reducing the express key to make line shorter
     [ Finishes #104714660] reducing the express key to make line shorter
    
     [ Finishes #104714660] reducing the express key to make line shorter

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,8 @@
+var expressKey = process.env.EXPRESS_SESSION_KEY,
+customKey = '436b2efb-0302-4113-9954-f658f554ea87';
+
 var envVariables = {
-    expressSessionKey: process.env.EXPRESS_SESSION_KEY || '436b2efb-0302-4113-9954-f658f554ea87',
+    expressSessionKey:expressKey || customKey,
     database: process.env.DATABASE_NAME ||'vvida',
     host: process.env.HOST || 'localhost',
     userName: process.env.USER_NAME || 'postgres',
