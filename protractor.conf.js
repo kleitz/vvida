a152e7b25c6b151accd30ddb6f9a1db88957fa5e
andela-nmartin <godmetweenciati@gmail.com>
2015-10-02 12:28:31 +0300

    [Fixes #102245938] broke long lines causing houndci errors

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,8 @@
 // https://raw.github.com/angular/protractor/master/example/conf.js
 exports.config = {
   // The address of a running selenium server.
-  seleniumServerJar: '../node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.44.0.jar', 
+  seleniumServerJar: '../node_modules/selenium-server-standalone-jar/' + 
+      'jar/selenium-server-standalone-2.44.0.jar', 
   // Make use you check the version in the folder
   allScriptsTimeout: 11000,
   chromeDriver: '../node_modules/chromedriver/bin/chromedriver',
