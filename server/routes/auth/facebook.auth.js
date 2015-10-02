b3d59e85c3ff9db04faad7ca9c44c9f011a2afce
Eugene Mutai <eugene.mutai@andela.com>
2015-10-02 17:39:21 +0300

    [#104002586] Shortened the routing description, fixed user model queries and wrapped the social auth success callback in a process

--- server/routes/auth/facebook.auth.js
+++ server/routes/auth/facebook.auth.js
@@ -3,26 +3,21 @@ module.exports = function(app, config, passport) {
   // Redirect the user to Facebook for authentication.  When complete,
   // Facebook will redirect the user back to the application at
   //     /auth/facebook/callback
-  app.route('/auth/facebook')
-    .get(passport.authenticate('facebook', {
-      scope: [
-        'email', 'user_about_me'
-        // 'basic_info', 'user_birthday', 'user_hometown', 'user_location'
-      ],
-      failureRedirect: '/signup',
-      session: false
-    }));
+  app.get('/auth/facebook', passport.authenticate('facebook', {
+    scope: [
+      'email', 'user_about_me'
+      // 'basic_info', 'user_birthday', 'user_hometown', 'user_location'
+    ]
+  }));
 
   // Facebook will redirect the user to this URL after approval.
   // Finish the authentication process by attempting to obtain an access token.
   // If access was granted, the user will be logged in.
   // Otherwise, authentication has failed.
-  app.route('/auth/facebook/callback')
-    .get(passport.authenticate('facebook', {
-      failureRedirect: '/login',
-      session: false
-    }), function(req, res) {
-      res.redirect('/');
-    });
+  app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/sign-up',
+    successRedirect: '/'
+      // session: false
+  }));
 
 };
