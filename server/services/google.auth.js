3b1331c9f7b8058f1f455847eebdf68bfec90024
Eugene Mutai <eugene.mutai@andela.com>
2015-10-02 17:41:17 +0300

    [#104002586] Shortened the routing description, fixed user model queries and wrapped Google social auth success callback in a process

--- server/services/google.auth.js
+++ server/services/google.auth.js
@@ -8,41 +8,66 @@ module.exports = function(passport, GoogleStrategy) {
   passport.use(new GoogleStrategy({
       clientID: '893360311572-s0cnhs0ffojknftltkmm9cbj1p078d7o.apps.googleusercontent.com',
       clientSecret: 'jDV73toE_qjEkjg32p8dNwZ7',
-      callbackURL: 'http://localhost:3000/auth/google/callback',
-      // returnURL: 'http://localhost:3000/auth/google/callback',
-      // realm: 'http://localhost:3000/'
+      callbackURL: 'http://localhost:3000/auth/google/callback'
+        // returnURL: 'http://localhost:3000/auth/google/callback',
+        // realm: 'http://localhost:3000/',
+        // passReqToCallback: true
     },
     function(accessToken, refreshToken, profile, done) {
-      User.sync();
-      // check if the user exists in out database
-      User.findOne({
-          'google_auth_id': profile.id
-        },
-        function(err, user) {
-          // If the user does not exist create one
-          if (!user) {
-            User
-              .build({
-                email: profile.emails[0].value,
-                role: 'user',
-                username: profile.username
+      console.log(profile);
+
+      // make the code asynchronous
+      // User.findOne won't fire until we have all our data back from Google
+      process.nextTick(function() {
+
+        // check if the user exists in out database
+        User.findOne({
+            where: {
+              'google_auth_id': profile.id
+            },
+            attributes: ['id', 'firstname', 'lastname', 'google_auth_id']
+          })
+          .then(function(user) {
+            // If the user does not exist create one
+            if (!user) {
+              console.log("The user does not exist.");
+              User.build({
+                  email: profile.emails[0].value,
+                  role: 'user',
+                  username: profile.username,
+                  google_auth_id: profile.id,
+                  google_auth_token: accessToken,
                   // provider: 'facebook',
                   // facebook: profile._json
-              })
-              .fullName(profile.displayName)
-              .save()
-              .then(function(user) {
-                done(null, user);
-              })
-              .catch(function(err) {
-                if (err) {
-                  return done(err);
-                }
-              });
-          } else {
-            return done(err, user);
-          }
-        });
+                })
+                // set their name
+                .setFullName(profile.displayName)
+                // save the user instance build
+                .save()
+                .then(function(user) {
+                  done(null, user);
+                })
+                .catch(function(err) {
+                  if (err) {
+                    return done(err);
+                  }
+                });
+            }
+            // If the user was found, then just do a redirect
+            else {
+              // or TODO maybe create cookies/sessions
+              console.log("The user exits, just redirecting them", user);
+              done(null, profile);
+            }
+          })
+          .catch(function(err) {
+            if (err) {
+              return done(err);
+            }
+          });
+
+      });
+
     }
   ));
 
