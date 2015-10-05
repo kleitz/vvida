434e69c3709f3565cf31e05a88de32941212dfbc
Hannah Koske <hannah.koske@andela.com>
2015-10-05 15:16:41 +0300

    [#103539256] Fixed changes in services.js  made during testing

--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -1,12 +1,8 @@
 angular.module('vvida.services', []).service('Utils', function($mdToast, $mdDialog) {
   this.toast = function(msg) {
     $mdToast.show($mdToast.simple().content(msg));
-    // Could also do $mdToast.showSimple('Hello');
   };
   this.dialog = function(title, message, event, callback) {
-    // Appending dialog to document.body to cover sidenav in docs app
-    // Modal dialogs should fully cover application
-    // to prevent interaction outside of dialog
     $mdDialog.show(
         $mdDialog.confirm()
         .parent(angular.element(document.body))
