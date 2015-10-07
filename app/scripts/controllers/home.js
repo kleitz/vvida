8635daf2a768236bfec67c4d800879e86e882887
andela-thomas <thomas.nyambati@andela.com>
2015-10-07 09:52:38 +0300

    [#105076718] Resolved scope in login controller

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,7 +1,12 @@
 angular.module('vvida.controllers')
   .controller('HomeCtrl', ['$scope', function($scope) {
-    $scope.header_image = "images/vvidaLogo.png";
+    $scope.header_image = 'images/vvidaLogo.png';
   }])
   .controller('LoginCtrl', ['$scope', function($scope) {
-
+    $scope.userData = {
+      firstName :'',
+      lastName :'',
+      email :'',
+      password :''
+    };
   }]);
