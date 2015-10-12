feef45fb9c3b7be34cff879fac139c87a7678326
Eugene Mutai <eugenemutai@gmail.com>
2015-10-12 09:38:59 +0300

    [Chore #105439814] Fixed houndci issue LN:10 - Line is too long

--- server/config/index.js
+++ server/config/index.js
@@ -1,13 +1,14 @@
 var expressKey = process.env.EXPRESS_SESSION_KEY,
-customKey = '436b2efb-0302-4113-9954-f658f554ea87';
+  customKey = '436b2efb-0302-4113-9954-f658f554ea87';
 
 var envVariables = {
-    expressSessionKey:expressKey || customKey,
-    database: process.env.DATABASE_NAME ||'vvida',
+    expressSessionKey: expressKey || customKey,
+    database: process.env.DATABASE_NAME || 'vvida',
     host: process.env.HOST || 'localhost',
     userName: process.env.USER_NAME || 'postgres',
     password: process.env.PASSWORD || 'root',
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || '893360311572-s0cnhs0ffojknftltkmm9cbj1p078d7o.apps.googleusercontent.com'
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET ||
+      '893360311572-s0cnhs0ffojknftltkmm9cbj1p078d7o.apps.googleusercontent.com'
   },
   development = {
     expressSessionKey: envVariables.expressSessionKey,
@@ -15,7 +16,7 @@ var envVariables = {
     host: envVariables.host,
     username: envVariables.userName,
     password: envVariables.password,
-    googleClientSecret:envVariables.googleClientSecret
+    googleClientSecret: envVariables.googleClientSecret
   },
   staging = {
     expressSessionKey: envVariables.expressSessionKey,
@@ -23,7 +24,7 @@ var envVariables = {
     host: envVariables.host,
     username: envVariables.userName,
     password: envVariables.password,
-    googleClientSecret:envVariables.googleClientSecret
+    googleClientSecret: envVariables.googleClientSecret
   },
   production = {
     expressSessionKey: envVariables.expressSessionKey,
@@ -31,7 +32,7 @@ var envVariables = {
     host: envVariables.host,
     username: envVariables.userName,
     password: envVariables.password,
-    googleClientSecret:envVariables.googleClientSecret
+    googleClientSecret: envVariables.googleClientSecret
   };
 
 module.exports = {
