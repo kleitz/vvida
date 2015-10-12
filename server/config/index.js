446fea3328b05405cc1c4475f09a39ffb2a3f365
Eugene Mutai <eugenemutai@gmail.com>
2015-10-12 11:12:21 +0300

    [Fixes #104899696] Refactor: wrongly placed passport auth configuration, now moved to envVariables.auth

--- server/config/index.js
+++ server/config/index.js
@@ -1,20 +1,20 @@
 var expressKey = process.env.EXPRESS_SESSION_KEY,
   customKey = '436b2efb-0302-4113-9954-f658f554ea87',
+  passportConfig = require('./passport.config'),
   envVariables = {
     expressSessionKey: expressKey || customKey,
     database: process.env.DATABASE_NAME || 'vvida',
     host: process.env.HOST || 'localhost',
     userName: process.env.USER_NAME || 'postgres',
-    password: process.env.PASSWORD || 'root'
+    password: process.env.PASSWORD || 'root',
+    auth: passportConfig
   },
   development = envVariables,
   staging = envVariables,
-  production = envVariables,
-  passportConfig = require('./passport.config');
+  production = envVariables;
 
 module.exports = {
   development: development,
   staging: staging,
   production: production,
-  auth: passportConfig,
 };
