25f57c7c6d909d0b62d493ac481a395dfedad1b8
andela-nmartin <godmetweenciati@gmail.com>
2015-10-12 12:34:45 +0300

    [Fixes #102245952] testing definition of vvida

--- tests/unit/serverspec/servertest.spec.js
+++ tests/unit/serverspec/servertest.spec.js
@@ -1,18 +1,18 @@
 describe('Testing Vvida Home Controller', function() {
-  var _scope, HomeController;
+  // var _scope, HomeController;
 
   beforeEach(function() {
-    module('vvida');
+    mainModule = angular.module('vvida');
 
-    inject(function($rootScope, $controller) {
-      _scope = $rootScope.$new();
-      HomeController = $controller('HomeCtrl', {
-        $scope: _scope
-      });
-    });
+    // inject(function($rootScope, $controller) {
+    //   _scope = $rootScope.$new();
+    //   HomeController = $controller('HomeCtrl', {
+    //     $scope: _scope
+    //   });
+    // });
   });
 
   it('should be registered', function() {
-    expect(HomeController).toBeDefined();
+    expect(mainModule).toBeDefined();
   });
 });
