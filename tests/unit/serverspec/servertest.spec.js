d4a0168ea4d2921d18a381423fbde87a3d8b8f61
andela-nmartin <godmetweenciati@gmail.com>
2015-10-12 14:56:00 +0300

    [Fixes #102245952] changed app name in module

--- tests/unit/serverspec/servertest.spec.js
+++ tests/unit/serverspec/servertest.spec.js
@@ -1,19 +1,43 @@
-describe('Testing Vvida Home Controller', function() {
+describe('App module', function() {
   // var _scope, HomeController;
   var mainModule;
 
   beforeEach(function() {
-    mainModule = angular.module('vvida');
-
-    // inject(function($rootScope, $controller) {
-    //   _scope = $rootScope.$new();
-    //   HomeController = $controller('HomeCtrl', {
-    //     $scope: _scope
-    //   });
-    // });
+    mainModule = angular.module('App');
   });
 
   it('should be registered', function() {
     expect(mainModule).not.toEqual(null);
   });
+  describe("Dependencies:", function() {
+
+      var deps;
+      var hasModule = function(m) {
+        return deps.indexOf(m) >= 0;
+      };
+      before(function() {
+        deps = module.value('App').requires;
+      });
+
+      //you can also test the module's dependencies
+      it("should have App.Controllers as a dependency", function() {
+        expect(hasModule('App.Controllers')).toEqual(true);
+      });
+
+      it("should have App.Directives as a dependency", function() {
+        expect(hasModule('App.Directives')).toEqual(true);
+      });
+
+      it("should have App.Filters as a dependency", function() {
+        expect(hasModule('App.Filters')).toEqual(true);
+      });
+
+      it("should have App.Routes as a dependency", function() {
+        expect(hasModule('App.Routes')).toEqual(true);
+      });
+
+      it("should have App.Services as a dependency", function() {
+        expect(hasModule('App.Services')).to.equal(true);
+      });
+    });
 }); 
