8830a891c18edb9d18eb91024ef83f98046cfa08
Eugene Mutai <eugenemutai@gmail.com>
2015-10-12 10:50:40 +0300

    [Finishes #104899696] Refactored the config auth index file, condensing it by removing redundancies

--- server/config/index.js
+++ server/config/index.js
@@ -1,42 +1,20 @@
 var expressKey = process.env.EXPRESS_SESSION_KEY,
-  customKey = '436b2efb-0302-4113-9954-f658f554ea87';
-
-var envVariables = {
+  customKey = '436b2efb-0302-4113-9954-f658f554ea87',
+  envVariables = {
     expressSessionKey: expressKey || customKey,
     database: process.env.DATABASE_NAME || 'vvida',
     host: process.env.HOST || 'localhost',
     userName: process.env.USER_NAME || 'postgres',
-    password: process.env.PASSWORD || 'root',
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET ||
-      '893360311572-s0cnhs0ffojknftltkmm9cbj1p078d7o.apps.googleusercontent.com'
+    password: process.env.PASSWORD || 'root'
   },
-  development = {
-    expressSessionKey: envVariables.expressSessionKey,
-    database: envVariables.database,
-    host: envVariables.host,
-    username: envVariables.userName,
-    password: envVariables.password,
-    googleClientSecret: envVariables.googleClientSecret
-  },
-  staging = {
-    expressSessionKey: envVariables.expressSessionKey,
-    database: envVariables.database,
-    host: envVariables.host,
-    username: envVariables.userName,
-    password: envVariables.password,
-    googleClientSecret: envVariables.googleClientSecret
-  },
-  production = {
-    expressSessionKey: envVariables.expressSessionKey,
-    database: envVariables.database,
-    host: envVariables.host,
-    username: envVariables.userName,
-    password: envVariables.password,
-    googleClientSecret: envVariables.googleClientSecret
-  };
+  development = envVariables,
+  staging = envVariables,
+  production = envVariables,
+  passportConfig = require('./passport.config');
 
 module.exports = {
   development: development,
   staging: staging,
   production: production,
+  auth: passportConfig,
 };
