8971a3e7d537a6cdeac9b800468589b06509dfe4
Eugene Mutai <eugenemutai@gmail.com>
2015-10-12 16:10:26 +0300

    [#103855610] Added user resource route tests

--- server/tests/users.spec.js
+++ server/tests/users.spec.js
@@ -1,14 +1,14 @@
-var request = require('superagent'),
-  requestPrefix = require('superagent-prefix')('/api');
+var request = require('superagent');
 
 describe('User RESTful API tests', function() {
 
   var fakeUser = {
     username: 'eugenemutai',
     password: 'vvidaTesting',
+    email: 'eugenemutai@gmail.com',
     firstname: 'Eugene',
     lastname: 'Mutai',
-    gender: 'male'
+    gender: 'male',
     dob: '30081989',
     country: 'Kenya',
     city: 'Nairobi'
@@ -20,8 +20,26 @@ describe('User RESTful API tests', function() {
    *
    * @return Response
    */
-  it('should return all users stored in the database or empty array if DB is empty', function() {
-
+  it('should return all users stored in the database or empty array if DB is empty', function(done) {
+    request
+      .get('http://localhost:3000/api/users')
+      // .use(requestPrefix)
+      .set('Content-Type', 'application/json')
+      // .expect('Content-Type', /json/)
+      // .expect(200)
+      .end(function(err, res) {
+        // console.log(res);
+        if (res.status == 200) {
+          if (res.body.length == 0) {
+            expect(Object.prototype.toString.call(res.body)).toBe('[object Array]');
+          } else {
+            expect(res.body.length).toBeGreaterThan(0);
+            expect(typeof res.body[0].id).toBe('number');
+            expect(typeof res.body[0].password).toBe('string');
+          }
+          done();
+        }
+      });
   });
 
   /**
@@ -30,8 +48,25 @@ describe('User RESTful API tests', function() {
    *
    * @return Response
    */
-  it('should show the form for creating a new resource.', function() {
-
+  it('should store a newly created resource in storage.', function(done) {
+    request
+      .post('http://localhost:3000/api/users')
+      .send(fakeUser)
+      .accept('application/json')
+      // .expect('Content-Type', /json/)
+      // .expect(200)
+      .end(function(err, res) {
+        if (res.status == 200) {
+          var data = res.body;
+          // console.log(data);
+          expect(data.email).toBe(fakeUser.email);
+          expect(data.id).toBeDefined();
+          expect(typeof data.id).toBe('number');
+        } else {
+          return new Error();
+        }
+        done();
+      });
   });
 
   /**
@@ -55,7 +90,20 @@ describe('User RESTful API tests', function() {
    * @param  int  $id
    * @return Response
    */
-  it('should store a newly created resource in storage.', function() {
+  it('should store a newly created resource in storage.', function(done) {
+    request
+      .get('http://localhost:3000/api/users/1')
+      .accept('application/json')
+      // .expect('Content-Type', /json/)
+      // .expect(200)
+      .end(function(err, res) {
+        if (res.status == 200) {
+          expect(JSON.parse(res.text).id).toBe(1);
+        } else {
+          expect(res.text).toMatch(/(not found)/g);
+        }
+        done();
+      });
 
   });
 
@@ -81,7 +129,19 @@ describe('User RESTful API tests', function() {
    * @return Response
    */
   it('should update the specified resource in storage.', function() {
-
+    request
+      .put('http://localhost:3000/api/users/1')
+      .accept('application/json')
+      // .expect('Content-Type', /json/)
+      // .expect(200)
+      .end(function(err, res) {
+        if (res.status == 200) {
+          expect(res.text).toMatch(/(success)/);
+        } else {
+          expect(res.status).toBe(501);
+        }
+        done();
+      });
   });
 
   /**
@@ -91,7 +151,15 @@ describe('User RESTful API tests', function() {
    * @param  int  $id
    * @return Response
    */
-  it('should remove the specified resource from storage.', function() {
-
+  it('should remove the specified resource from storage.', function(done) {
+    request
+      .del('http://localhost:3000/api/users/1')
+      .accept('application/json')
+      // .expect('Content-Type', /json/)
+      // .expect(200)
+      .end(function(err, res) {
+        expect(res.status).toBe(501);
+        done();
+      });
   });
 });
