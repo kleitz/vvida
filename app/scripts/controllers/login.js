b59bcf81849593399717ec8d3697c97c95ed0b68
andela-nmartin <godmetweenciati@gmail.com>
2015-10-13 16:04:40 +0300

    [Chore #105547976] added Auth to login controller

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,6 +1,7 @@
 angular.module('vvida.controllers')
-  .controller('LoginCtrl', ['$scope', 'Users', function($scope, Users) {
+  .controller('LoginCtrl', ['$scope', 'Users', 'Auth', function($scope, Users) {
     $scope.login = function() {
+      Auth.setUser(user); //Update the state of the user in the app
       Users.login($scope.user, function(err, res) {
         console.log(err, res);
       });
