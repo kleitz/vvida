ab6c3d6e33d46723643cd72cddfe284be7646ad0
Teddy Otieno <teddy.asola@andela.com>
2015-10-14 14:49:26 +0300

    [#105644072] Added Hamburger controller for the SideNav

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,3 +2,10 @@ angular.module('vvida.controllers')
   .controller('HomeCtrl', ['$scope', function($scope) {
     $scope.header_image = 'images/vvidaLogo.png';
   }]);
+
+angular.module('vvida.controllers')
+  .controller('HamburgerCtrl', function($scope, $mdSidenav) {
+    $scope.openLeftMenu = function() {
+      $mdSidenav('left').toggle();
+    };
+  });
