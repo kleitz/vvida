7f68f4662b79ec0fcd961c105f35948cab22aef6
Teddy Otieno <teddy.asola@andela.com>
2015-10-14 17:39:46 +0300

    [#105644072] Refactored SideNav files

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,10 +2,3 @@ angular.module('vvida.controllers')
   .controller('HomeCtrl', ['$scope', function($scope) {
     $scope.header_image = 'images/vvidaLogo.png';
   }]);
-
-angular.module('vvida.controllers')
-  .controller('HamburgerCtrl', function($scope, $mdSidenav) {
-    $scope.openLeftMenu = function() {
-      $mdSidenav('left').toggle();
-    };
-  });
