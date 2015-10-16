bfd8e284895bc6f21d1f0d42237e8fa0cda51917
andela-thomas <thomas.nyambati@andela.com>
2015-10-16 16:39:32 +0300

    [#104514732] adds multer upload

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,21 +2,22 @@
 // this api will handle all the routes for events
 var cloudinary = require('cloudinary'),
   multer = require('multer');
-var upload = multer({
-  dest: './uploads/'
-}).single('photo');
+// var upload = multer({
+//   dest: '/Users/Andela8/projects/vvida/uploads/'
+// }).single('photo');
 
+var upload = multer({
+  dest: 'uploads/'
+});
 module.exports = function(app) {
   app.route('/api/upload')
-
-    .post(function(req, res, next) {
-      console.log(req.files);
-      upload(req, res, function(err) {
-        if (err) {
-          return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
+    .post(upload.fields([{
+      name: 'photo',
+      maxCount: 1
+    }]), function(req, res, next) {
+      res.send({
+        files: req.files,
+        file: req.file
       });
     });
-
 };
