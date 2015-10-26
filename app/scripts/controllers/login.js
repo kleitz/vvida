d3af63747f74a9106a57708c3e295dd3795898a6
Eugene Mutai <eugenemutai@gmail.com>
2015-10-26 15:27:18 +0300

    [Fixes #105995884] Refactors: houndci issues

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,21 +1,23 @@
 angular.module('vvida.controllers')
-  .controller('LoginCtrl', ['$rootScope', '$scope', '$cookies', '$state', 'Users', function($rootScope, $scope, $cookies, $state, Users) {
-    // login
-    $scope.login = function() {
-      Users.login($scope.user, function(err, res) {
-        console.log('Err: ', err, 'Res ', res);
-        if (!err) {
-          $rootScope.currentUser = res;
-          $state.go('home');
-        } else {
-          $scope.message = err.error || err;
-        }
-      });
-    };
-    // signup
-    $scope.signup = function() {
-      Users.save($scope.user, function(err, user) {
-        console.log(user, err);
-      });
-    };
-  }]);
+  .controller('LoginCtrl', ['$rootScope', '$scope', '$state', 'Users',
+    function($rootScope, $scope, $state, Users) {
+      // login
+      $scope.login = function() {
+        Users.login($scope.user, function(err, res) {
+          console.log('Err: ', err, 'Res ', res);
+          if (!err) {
+            $rootScope.currentUser = res;
+            $state.go('home');
+          } else {
+            $scope.message = err.error || err;
+          }
+        });
+      };
+      // signup
+      $scope.signup = function() {
+        Users.save($scope.user, function(err, user) {
+          console.log(user, err);
+        });
+      };
+    }
+  ]);
