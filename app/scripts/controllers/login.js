81d4310cb32ec4e7339f36e398dcf0fd22b71d5c
Hannah Koske <hannah.koske@andela.com>
2015-10-26 12:18:11 +0300

    [#105995884]Added cookie to login controller

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,11 +1,19 @@
 angular.module('vvida.controllers')
-  .controller('LoginCtrl', ['$rootScope', '$scope', '$state', 'Users', function($rootScope, $scope, $state, Users) {
+  .controller('LoginCtrl', ['$rootScope', '$scope', '$cookies', '$state', 'Users', function($rootScope, $scope, $cookies, $state, Users) {
     // login
     $scope.login = function() {
       Users.login($scope.user, function(err, res) {
         console.log('Err: ', err, 'Res ', res);
         if (!err) {
-          $rootScope.currentUser = res;
+          var storeUser = {
+            id: res.id,
+            firstname: res.firstname,
+            lastname: res.lastname,
+            pictureUrl: res.picture_url,
+            email: res.email
+          };
+          $cookies.put('vvidaUserPersisted', JSON.stringify(storeUser));
+          $rootScope.currentUser = $cookies.get('vvidaUserPersisted');
           $state.go('home');
         } else {
           $scope.message = err.error || err;
