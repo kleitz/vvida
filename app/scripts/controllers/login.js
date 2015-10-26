1256502bf86cc5989e0158bc15f181c91ce39ee5
Eugene Mutai <eugenemutai@gmail.com>
2015-10-26 11:02:44 +0300

    [#105995884] Persisted user login data before redirection

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,10 +1,11 @@
 angular.module('vvida.controllers')
-  .controller('LoginCtrl', ['$state','$scope', 'Users', function($state,$scope, Users) {
+  .controller('LoginCtrl', ['$rootScope', '$scope', '$state', 'Users', function($rootScope, $scope, $state, Users) {
     // login
     $scope.login = function() {
       Users.login($scope.user, function(err, res) {
         console.log('Err: ', err, 'Res ', res);
-        if(!err) {
+        if (!err) {
+          $rootScope.currentUser = res;
           $state.go('home');
         } else {
           $scope.message = err.error || err;
