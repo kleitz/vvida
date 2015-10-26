c3e1cfa888cdc4273bc052e40f0f7846bc6b320d
Hannah Koske <hannah.koske@andela.com>
2015-10-26 12:16:28 +0300

    [#105995884]Added cookie to header controller

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -1,10 +1,11 @@
 angular.module('vvida.controllers')
-  .controller('HeaderCtrl', ['$rootScope', '$scope', '$state', 'Users', function($rootScope, $scope, $state, Users) {
+  .controller('HeaderCtrl', ['$rootScope', '$scope', '$cookies', '$state', 'Users', function($rootScope, $scope, $cookies, $state, Users) {
     // logout
     $scope.logout = function() {
       Users.logout($scope.user, function(err, res) {
         if (!err) {
-          $rootScope.currentUser = {};
+          $rootScope.currentUser = undefined;
+          $cookies.remove('vvidaUserPersisted');
           $state.go('login');
         } else {
           console.log(err, res);
