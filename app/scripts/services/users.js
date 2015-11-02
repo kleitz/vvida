37b92923c9889ebafd875ba6cc268923683504b3
Hannah Koske <hannah.koske@andela.com>
2015-11-02 18:36:59 +0300

    [#106817140] Adds countries method to Users service

--- app/scripts/services/users.js
+++ app/scripts/services/users.js
@@ -27,6 +27,15 @@ angular.module('vvida.services')
       });
     };
 
+    obj.countries = function(cb){
+      $http.get('/api/countries').success(function(res) {
+        cb(null, res);
+      }).error(function(err) {
+        cb(err);
+      });
+    };
+
+
     obj.logout = function(user, cb) {
       $http.get('/api/users/logout', user).success(function(res) {
         cb(null, res);
