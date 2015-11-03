9350e4d9975caf93e6e6cda5ebd5622e28008ab9
Hannah Koske <hannah.koske@andela.com>
2015-11-03 10:00:01 +0300

    [#106817140] Removes err and res as parameters in Users.save callbacks, reduces line 20 length and removes injection Users

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -1,13 +1,13 @@
 angular.module('vvida.controllers')
-  .controller('ProfileCtrl', ['$rootScope', '$scope', '$state', 'Users', 'Utils',
-    function($rootScope, $scope, $state, Users, Utils) {
+  .controller('ProfileCtrl', ['$rootScope', '$scope', '$state', 'Users',
+    function($rootScope, $scope, $state, Users) {
       if ($rootScope.currentUser) {
         Users.countries(function(err, res){
           if(res){
           $scope.countries = res;
         }
         else{
-          $scope.countries = [{name: 'No countries available'}];
+          $scope.countries = [{name: 'No countries available.'}];
         }
         });
         $scope.user = {};
@@ -17,16 +17,18 @@ angular.module('vvida.controllers')
 
         $scope.editProfile = function() {
           $scope.user.password = $rootScope.currentUser.password;
-          $scope.user.dob = new Date(($scope.theDate).getUTCFullYear(), ($scope.theDate).getUTCMonth(), ($scope.theDate).getUTCDate(), ($scope.theDate).getUTCHours(), ($scope.theDate).getUTCMinutes(), ($scope.theDate).getUTCSeconds());
+          $scope.user.dob = new Date(($scope.theDate).getUTCFullYear(), ($scope.theDate).getUTCMonth(),
+            ($scope.theDate).getUTCDate(), ($scope.theDate).getUTCHours(), ($scope.theDate).getUTCMinutes(),
+            ($scope.theDate).getUTCSeconds());
           if (!$rootScope.currentUser) {
             $state.go('404');
           }
           else {
-            Users.update($scope.user, function(res) {
+            Users.update($scope.user, function() {
               $rootScope.currentUser = $scope.user;
-              $scope.message = 'You have successfully updated your profile. Click on the home button to get to vvida homepage';
-            }, function(err) {
-              $scope.message = 'There was a problem updating your profile';
+              $scope.message = 'You have successfully updated your profile. Click on the home button to get to vvida homepage.';
+            }, function() {
+              $scope.message = 'There was a problem updating your profile.';
             });
           }
 
