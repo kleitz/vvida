123368a10469e4441c712fcf1bdd480611167981
Hannah Koske <hannah.koske@andela.com>
2015-11-03 16:31:13 +0300

    [#106817140] Replaces replace numerous new Date() arguments with .theDate and adds .currentUser.country as an OR value to .user.country

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -11,36 +11,22 @@ angular.module('vvida.controllers')
             }];
           }
         });
-
-        $scope.user = {
-          id: $rootScope.currentUser.id,
-          country: 'Kenya',
-          email: $rootScope.currentUser.email
-        };
+        $scope.user = {};
+        $scope.user.id = $rootScope.currentUser.id;
+        $scope.user.country = $rootScope.currentUser.country || 'Kenya';
+        $scope.user.email = $rootScope.currentUser.email;
 
         $scope.editProfile = function() {
           $scope.user.password = $rootScope.currentUser.password;
-
-          var Year = ($scope.theDate).getUTCFullYear(),
-            Month = ($scope.theDate).getUTCMonth(),
-            Day = ($scope.theDate).getUTCDate(),
-            Hour = ($scope.theDate).getUTCHours(),
-            min = ($scope.theDate).getUTCMinutes(),
-            seconds = ($scope.theDate).getUTCSeconds();
-          $scope.user.dob = new Date(Year, Month, Day, Hour, min, seconds);
-
-          if (!$rootScope.currentUser) {
-            $state.go('404');
-          } else {
-            Users.update($scope.user, function() {
-              $rootScope.currentUser = $scope.user;
-              $scope.message =
-                'You have successfully updated your profile. Click on the home button to get to vvida homepage.';
-            }, function() {
-              $scope.message =
-                'There was a problem updating your profile.';
-            });
-          }
+          $scope.user.dob = new Date($scope.theDate);
+          Users.update($scope.user, function() {
+            $rootScope.currentUser = $scope.user;
+            $scope.message =
+              'You have successfully updated your profile. Click on the home button to get to vvida homepage.';
+          }, function() {
+            $scope.message =
+              'There was a problem updating your profile.';
+          });
         };
       } else {
         $state.go('login');
