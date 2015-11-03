6fc66203ebe7460fb1a366b96f16ae53721ed06a
Hannah Koske <hannah.koske@andela.com>
2015-11-03 11:06:49 +0300

    [#106817140] Reduces long line in edit-profile message

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -2,13 +2,14 @@ angular.module('vvida.controllers')
   .controller('ProfileCtrl', ['$rootScope', '$scope', '$state', 'Users',
     function($rootScope, $scope, $state, Users) {
       if ($rootScope.currentUser) {
-        Users.countries(function(err, res){
-          if(res){
-          $scope.countries = res;
-        }
-        else{
-          $scope.countries = [{name: 'No countries available.'}];
-        }
+        Users.countries(function(err, res) {
+          if (res) {
+            $scope.countries = res;
+          } else {
+            $scope.countries = [{
+              name: 'No countries available.'
+            }];
+          }
         });
         $scope.user = {};
         $scope.user.id = $rootScope.currentUser.id;
@@ -17,18 +18,17 @@ angular.module('vvida.controllers')
 
         $scope.editProfile = function() {
           $scope.user.password = $rootScope.currentUser.password;
-          $scope.user.dob = new Date(($scope.theDate).getUTCFullYear(), ($scope.theDate).getUTCMonth(),
-            ($scope.theDate).getUTCDate(), ($scope.theDate).getUTCHours(), ($scope.theDate).getUTCMinutes(),
-            ($scope.theDate).getUTCSeconds());
+          $scope.user.dob = new Date(($scope.theDate).getUTCFullYear(), ($scope.theDate).getUTCMonth(), ($scope.theDate).getUTCDate(), ($scope.theDate).getUTCHours(), ($scope.theDate).getUTCMinutes(), ($scope.theDate).getUTCSeconds());
           if (!$rootScope.currentUser) {
             $state.go('404');
-          }
-          else {
+          } else {
             Users.update($scope.user, function() {
               $rootScope.currentUser = $scope.user;
-              $scope.message = 'You have successfully updated your profile. Click on the home button to get to vvida homepage.';
+              $scope.message =
+                'You have successfully updated your profile. Click on the home button to get to vvida homepage.';
             }, function() {
-              $scope.message = 'There was a problem updating your profile.';
+              $scope.message =
+                'There was a problem updating your profile.';
             });
           }
 
