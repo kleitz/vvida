30e3db794ecfb0d788e00fd2bf3160639445537a
Eugene Mutai <eugenemutai@gmail.com>
2015-11-03 11:53:56 +0300

    [Fixes #107278550] refactor: removed process.nextTick, not required

--- server/services/auth/facebook.auth.js
+++ server/services/auth/facebook.auth.js
@@ -8,52 +8,46 @@ module.exports = function(app, passport, config) {
 
   passport.use(new FacebookStrategy(config.auth.FACEBOOK,
     function(accessToken, refreshToken, profile, done) {
-      // make the code asynchronous
       // Users.findOne won't fire until we have all our data back from Facebook
-      process.nextTick(function() {
-
-        // check if the user exists in out database
-        Users.findOne({
-            where: {
-              'facebook_auth_id': profile.id
-            },
-            attributes: ['id', 'firstname', 'lastname', 'facebook_auth_id']
-          }).then(function(user) {
-            // If the user does not exist create one
-            if (!user) {
-              Users.build({
-                  email: profile.emails[0].value,
-                  role: 'user',
-                  username: profile.username,
-                  facebook_auth_id: profile.id,
-                  facebook_auth_token: accessToken,
-                  // provider: 'facebook',
-                  // facebook: profile._json
-                })
-                .setFullName(profile.displayName)
-                .save()
-                .then(function(user) {
-                  done(null, user);
-                })
-                .catch(function(err) {
-                  if (err) {
-                    return done(err);
-                  }
-                });
-            }
-            // The user was found, redirect to homepage
-            else {
-              done(null, profile);
-            }
-          })
-          .catch(function(err) {
-            if (err) {
-              return done(err);
-            }
-          });
-
-      });
-
+      // check if the user exists in out database
+      Users.findOne({
+          where: {
+            'facebook_auth_id': profile.id
+          },
+          attributes: ['id', 'firstname', 'lastname', 'facebook_auth_id']
+        }).then(function(user) {
+          // If the user does not exist create one
+          if (!user) {
+            Users.build({
+                email: profile.emails[0].value,
+                role: 'user',
+                username: profile.username,
+                facebook_auth_id: profile.id,
+                facebook_auth_token: accessToken,
+                // provider: 'facebook',
+                // facebook: profile._json
+              })
+              .setFullName(profile.displayName)
+              .save()
+              .then(function(user) {
+                done(null, user);
+              })
+              .catch(function(err) {
+                if (err) {
+                  return done(err);
+                }
+              });
+          }
+          // The user was found, redirect to homepage
+          else {
+            done(null, profile);
+          }
+        })
+        .catch(function(err) {
+          if (err) {
+            return done(err);
+          }
+        });
     }
   ));
 
--- server/services/auth/google.auth.js
+++ server/services/auth/google.auth.js
@@ -8,58 +8,50 @@ module.exports = function(app, passport, config) {
 
   passport.use(new GoogleStrategy(config.auth.GOOGLE,
     function(accessToken, refreshToken, profile, done) {
-      console.log(profile);
-
-      // make the code asynchronous
       // Users.findOne won't fire until we have all our data back from Google
-      process.nextTick(function() {
-
-        // check if the user exists in out database
-        Users.findOne({
-            where: {
-              'google_auth_id': profile.id
-            },
-            attributes: ['id', 'firstname', 'lastname', 'google_auth_id']
-          })
-          .then(function(user) {
-            // If the user does not exist create one
-            if (!user) {
-              Users.build({
-                  email: profile.emails[0].value,
-                  role: 'user',
-                  username: profile.username,
-                  google_auth_id: profile.id,
-                  google_auth_token: accessToken,
-                  // provider: 'facebook',
-                  // facebook: profile._json
-                })
-                // set their name
-                .setFullName(profile.displayName)
-                // save the user instance build
-                .save()
-                .then(function(user) {
-                  done(null, user);
-                })
-                .catch(function(err) {
-                  if (err) {
-                    return done(err);
-                  }
-                });
-            }
-            // If the user was found, then just do a redirect
-            else {
-              // or TODO maybe create cookies/sessions
-              done(null, profile);
-            }
-          })
-          .catch(function(err) {
-            if (err) {
-              return done(err);
-            }
-          });
-
-      });
-
+      // check if the user exists in out database
+      Users.findOne({
+          where: {
+            'google_auth_id': profile.id
+          },
+          attributes: ['id', 'firstname', 'lastname', 'google_auth_id']
+        })
+        .then(function(user) {
+          // If the user does not exist create one
+          if (!user) {
+            Users.build({
+                email: profile.emails[0].value,
+                role: 'user',
+                username: profile.username,
+                google_auth_id: profile.id,
+                google_auth_token: accessToken,
+                // provider: 'facebook',
+                // facebook: profile._json
+              })
+              // set their name
+              .setFullName(profile.displayName)
+              // save the user instance build
+              .save()
+              .then(function(user) {
+                done(null, user);
+              })
+              .catch(function(err) {
+                if (err) {
+                  return done(err);
+                }
+              });
+          }
+          // If the user was found, then just do a redirect
+          else {
+            // or TODO maybe create cookies/sessions
+            done(null, profile);
+          }
+        })
+        .catch(function(err) {
+          if (err) {
+            return done(err);
+          }
+        });
     }
   ));
 
