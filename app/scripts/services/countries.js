a02f4f10b9d20878bd2b999a8cb98172060e1719
Hannah Koske <hannah.koske@andela.com>
2015-11-04 09:45:56 +0300

    [#106817140] Adds countries service

---
+++ app/scripts/services/countries.js
@@ -0,0 +1,11 @@
+angular.module('vvida.services')
+  .service('Countries', ['$http', function($http) {
+    this.getCountries = function(cb) {
+      $http.get('/api/countries').success(function(res) {
+        console.log('Here');
+        cb(null, res);
+      }).error(function(err) {
+        cb(err);
+      });
+    };
+  }]);
