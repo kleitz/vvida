b630ba43d4586096bfe06c5f816a2fbae8319482
Hannah Koske <hannah.koske@andela.com>
2015-11-09 09:22:29 +0300

    [#107275132] Adds get all, find, update and delete Review routes

--- server/routes/review.js
+++ server/routes/review.js
@@ -1,8 +1,11 @@
 var Reviews = require('../controllers/review');
 
-module.exports = function(app){
+module.exports = function(app) {
   app.route('/api/review')
     .post(Reviews.create)
-    .get(Reviews.all)
+    .get(Reviews.all);
+  app.route('/api/review/:id')
+    .get(Reviews.find)
     .put(Reviews.update)
+    .delete(Reviews.delete);
 };
