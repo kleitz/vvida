ec9aba716e4c461d9791ec063a997b9a33543174
Hannah Koske <hannah.koske@andela.com>
2015-11-09 07:40:33 +0300

    [#107286940] Adds userProfile controller

---
+++ app/scripts/controllers/userProfile.js
@@ -0,0 +1,16 @@
+angular.module('vvida.controllers')
+  .controller('UserProfileCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('UserEventsCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('UserReviewsCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('UserProductsCtrl', ['$scope', function($scope) {
+
+  }])
+  .controller('UserPicturesCtrl', ['$scope', function($scope) {
+
+  }]);
