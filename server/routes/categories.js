a960f37878f0fc1d0fc327fe46854a209a65fec0
Hannah Koske <hannah.koske@andela.com>
2015-11-09 09:21:00 +0300

    [#107275132] Adds get all, find, update and delete Categories routes

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,5 +1,12 @@
 var Categories = require('../controllers/categories');
 
 module.exports = function(app) {
-  app.route('/api/categories').post(Categories.create);
+  app.route('/api/categories')
+    .post(Categories.create)
+    .get(Categories.getAll);
+  app.route('/api/categories/:id')
+    .get(Categories.find)
+    .put(Categories.update)
+    .delete(Categories.delete);
+
 };
