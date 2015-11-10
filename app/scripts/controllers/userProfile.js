85eaa8911a860bd7660acdf1f1cda0c4d9d2c117
Hannah Koske <hannah.koske@andela.com>
2015-11-10 09:47:10 +0300

    [#107286940] Add main profile and user events  controllers

--- app/scripts/controllers/userProfile.js
+++ app/scripts/controllers/userProfile.js
@@ -1,15 +1,51 @@
 angular.module('vvida.controllers')
-  .controller('UserProfileCtrl', ['$scope', function($scope) {
+  .controller('MainProfileCtrl', ['$scope', '$rootscope', function($scope, $rootscope) {
+    if ($rootscope.currentUser) {
+      console.log('');
+    } else {
+      $state.go('login');
+    }
+  }])
+  .controller('UserProfileCtrl', ['$scope', '$rootscope', 'Users', function($scope, $rootscope, Users) {
 
   }])
-  .controller('UserEventsCtrl', ['$scope', function($scope) {
-
+  .controller('UserEventsCtrl', ['$scope', 'Users', function($scope, Users) {
+    Users.events(null, function(err, res) {
+      if (err) {
+        $scope.message = 'No events have been created.';
+      } else {
+        $scope.events = res;
+        console.log(res);
+      }
+    });
+    $scope.parseDate = function(date) {
+      return new Date(Date.parse(date)).toUTCString();
+    };
   }])
-  .controller('UserReviewsCtrl', ['$scope', function($scope) {
-
+  .controller('UserReviewsCtrl', ['$scope', 'Users', function($scope, Users) {
+    Users.reviews(null, function(err, res) {
+      if (err) {
+        $scope.message = 'No reviews have been made.';
+      } else {
+        $scope.reviews = res;
+      }
+    });
+    $scope.rateThis = function(num) {
+      var array = new Array(num);
+      for (var x = 0; x < array.length; x++) {
+        array[x] = 1;
+      }
+      return array;
+    };
   }])
-  .controller('UserProductsCtrl', ['$scope', function($scope) {
-
+  .controller('UserProductsCtrl', ['$scope', 'Users', function($scope, Users) {
+    Users.items(null, function(err, res) {
+      if (err) {
+        $scope.message = 'No products have been created.';
+      } else {
+        $scope.items = res;
+      }
+    });
   }])
   .controller('UserPicturesCtrl', ['$scope', function($scope) {
 
