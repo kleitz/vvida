f8e41c1369f5f859f54067fa1d78025d4bb94362
Eugene Mutai <eugenemutai@gmail.com>
2015-11-10 19:10:10 +0300

    [Fixes #107815860] Fix bend tests for items resource

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -4,26 +4,31 @@
   module.exports = {
     create: function(req, res) {
       var Items = req.app.get('models').Items;
-      Items.sync().then(function() {
-        return Items.create({
-          user_id: req.decoded.id,
-          category_id: req.body.catId,
-          name: req.body.itemName,
-          description: req.body.description
-        }).then(function(item) {
-          if (!item) {
-            res.status(500).send({
-              error: 'Failed to create item'
-            });
-          } else {
+      if (req.body.hasOwnProperty('itemName') && req.body.hasOwnProperty('description')) {
+        Items.create({
+            user_id: req.decoded.id,
+            category_id: req.body.catId,
+            name: req.body.itemName,
+            description: req.body.description
+          })
+          .then(function(item) {
+            if (!item) {
+              return res.status(500).send({
+                error: 'Failed to create item'
+              });
+            }
             res.json(item);
-          }
+          })
+          .catch(function(err) {
+            res.status(500).send({
+              error: err.message || err.errors[0].message
+            });
+          });
+      } else {
+        res.status(416).json({
+          error: 'All required values have not been provided to create the item.'
         });
-      }).catch(function(err) {
-        res.status(500).send({
-          error: err.message || err.errors[0].message
-        });
-      });
+      }
     },
 
     all: function(req, res) {
@@ -43,14 +48,13 @@
         where: {
           id: req.params.id
         }
-      }).then(function(item) {
-        if (!item) {
-          res.status(404).send({
+      }).then(function(item, err) {
+        if (err) {
+          return res.status(404).send({
             message: 'Item not found'
           });
-        } else {
-          res.json(item);
         }
+        res.json(item);
       }).catch(function(err) {
         res.status(500).send({
           error: err.message || err.errors[0].message
@@ -60,7 +64,7 @@
 
     update: function(req, res) {
       var Items = req.app.get('models').Items;
-      return Items.update(req.body, {
+      Items.update(req.body, {
         where: {
           id: req.params.id
         }
@@ -75,7 +79,7 @@
           message: 'Item has been updated.'
         });
       }).catch(function(err) {
-        res.status(500).send({
+        return res.status(500).send({
           error: err.message || err.errors[0].message
         });
       });
@@ -83,7 +87,7 @@
 
     delete: function(req, res) {
       var Items = req.app.get('models').Items;
-      return Items.destroy({
+      Items.destroy({
         where: {
           id: req.params.id
         }
@@ -93,7 +97,6 @@
             error: 'Delete failed'
           });
         }
-
         res.status(200).send({
           message: 'Delete successful'
         });
