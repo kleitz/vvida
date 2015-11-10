ffe87eae36a2db25d672fca731c3827126dd67c4
Hannah Koske <hannah.koske@andela.com>
2015-11-10 14:36:31 +0300

    [#107286940] Add events, items and review count to UserProfileCtrl and error handling in other controllers

--- app/scripts/controllers/userProfile.js
+++ app/scripts/controllers/userProfile.js
@@ -1,21 +1,40 @@
 angular.module('vvida.controllers')
-  .controller('MainProfileCtrl', ['$scope', '$rootscope', function($scope, $rootscope) {
-    if ($rootscope.currentUser) {
-      console.log('');
-    } else {
+  .controller('UserProfileCtrl', ['$scope', '$rootScope', '$state', 'Users', function($scope, $rootScope, $state, Users) {
+    if (!$rootScope.currentUser) {
       $state.go('login');
+    } else {
+      Users.eventsCount(function(err, res) {
+        if (err) {
+          $scope.eventsCount = 0;
+        } else {
+          $scope.eventsCount = res;
+        }
+      });
+      Users.itemsCount(function(err, res) {
+        if (err) {
+          $scope.itemsCount = 0;
+        } else {
+          $scope.itemsCount = res;
+        }
+      });
+      Users.reviewsCount(function(err, res) {
+        if (err) {
+          $scope.reviewsCount = 0;
+        } else {
+          $scope.reviewsCount = res;
+        }
+      });
     }
   }])
-  .controller('UserProfileCtrl', ['$scope', '$rootscope', 'Users', function($scope, $rootscope, Users) {
-
-  }])
   .controller('UserEventsCtrl', ['$scope', 'Users', function($scope, Users) {
-    Users.events(null, function(err, res) {
+    Users.events(function(err, res) {
       if (err) {
         $scope.message = 'No events have been created.';
       } else {
         $scope.events = res;
-        console.log(res);
+        if (res.Events.length === 0) {
+          $scope.message = 'No events have been created.';
+        }
       }
     });
     $scope.parseDate = function(date) {
@@ -23,11 +42,14 @@ angular.module('vvida.controllers')
     };
   }])
   .controller('UserReviewsCtrl', ['$scope', 'Users', function($scope, Users) {
-    Users.reviews(null, function(err, res) {
+    Users.reviews(function(err, res) {
       if (err) {
         $scope.message = 'No reviews have been made.';
       } else {
         $scope.reviews = res;
+        if (res.Reviews.length === 0) {
+          $scope.message = 'No reviews have been created.';
+        }
       }
     });
     $scope.rateThis = function(num) {
@@ -39,14 +61,23 @@ angular.module('vvida.controllers')
     };
   }])
   .controller('UserProductsCtrl', ['$scope', 'Users', function($scope, Users) {
-    Users.items(null, function(err, res) {
+    Users.items(function(err, res) {
       if (err) {
         $scope.message = 'No products have been created.';
       } else {
         $scope.items = res;
+        if (res.Items.length === 0) {
+          $scope.message = 'No products have been created.';
+        }
       }
     });
   }])
   .controller('UserPicturesCtrl', ['$scope', function($scope) {
-
+    $scope.arrayNo = function(num) {
+      var array = new Array(num);
+      for (var x = 0; x < array.length; x++) {
+        array[x] = 1;
+      }
+      return array;
+    };
   }]);
