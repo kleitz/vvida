06b2e3e6e1d56f8196525edef932e7c90289417e
andela-nmartin <godmetweenciati@gmail.com>
2015-11-11 17:00:17 +0300

    [#107793574] Adds users seeders

---
+++ seeders/users.js
@@ -0,0 +1,29 @@
+var moment = require('moment'),
+  bcrypt = require('bcrypt-nodejs');
+  
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      */
+    return queryInterface.bulkInsert('Users', [{
+      username: 'vvidaapp',
+      password: bcrypt.hashSync('3at1ngYums@wh1leD0ingTh3Whip.c0m'),
+      gender: 'female',
+      email: 'vvidaapp@gmail.com',
+      created_at: moment().utc().format(),
+      updated_at: moment().utc().format()
+    }], {});
+  },
+
+  down: function(queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
