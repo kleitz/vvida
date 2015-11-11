5a18ff39817cbe36cf76bb151e1f9722a8ad9156
andela-thomas <thomas.nyambati@andela.com>
2015-11-11 10:40:08 +0300

    [#107795522] Remove sync method plcaed before create Reviews method

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -4,22 +4,20 @@
   module.exports = {
     create: function(req, res) {
       var Reviews = req.app.get('models').Reviews;
-      Reviews.sync().then(function() {
-        return Reviews.create({
-          user_id: req.body.id,
-          item_id: req.body.itemId,
-          review: req.body.review,
-          review_title: req.body.reviewTitle,
-          rating: req.body.rating
-        }).then(function(review) {
-          if (!review) {
-            res.status(500).send({
-              error: 'Create review failed'
-            });
-          } else {
-            res.json(review);
-          }
-        });
+      return Reviews.create({
+        user_id: req.body.id,
+        item_id: req.body.itemId,
+        review: req.body.review,
+        review_title: req.body.reviewTitle,
+        rating: req.body.rating
+      }).then(function(review) {
+        if (!review) {
+          res.status(500).send({
+            error: 'Create review failed'
+          });
+        } else {
+          res.json(review);
+        }
       }).catch(function(err) {
         res.status(500).send({
           error: err.message || err.errors[0].message
