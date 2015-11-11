d7cb835c58b8b50798bbc25a990681313662e799
andela-thomas <thomas.nyambati@andela.com>
2015-11-11 10:30:01 +0300

    [#107795522] Add items and events scope variables to Home controller

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,4 +1,12 @@
 angular.module('vvida.controllers')
-  .controller('HomeCtrl', ['$scope', function($scope) {
+  .controller('HomeCtrl', ['$scope', 'Items', 'Events', function($scope, Items, Events) {
     $scope.header_image = 'images/vvidaLogo.png';
+
+    $scope.items = Items.query();
+
+    $scope.events = Events.query();
+    console.log($scope.items);
+
+
+
   }]);
