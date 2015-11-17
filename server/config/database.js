f0538da685a37852b838984ddb7accb091d0b4a1
Eugene Mutai <eugenemutai@gmail.com>
2015-11-17 11:48:56 +0300

    [Fixes #108224848] sequelizerc now autoloads database.js instead of the json. Thus adding environments from .env file or circleci env variables

---
+++ server/config/database.js
@@ -0,0 +1,19 @@
+var env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  require('dotenv').load();
+}
+
+var config = {
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  dialect: process.env.DATABASE_DIALECT,
+  port: process.env.DATABASE_PORT
+};
+
+module.exports = {
+  'development': config,
+  'test': config,
+  'production': config
+};
