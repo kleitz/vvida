7569ef07be36acdafa8706084d61ef4e4564fdf9
andela-thomas <thomas.nyambati@andela.com>
2015-11-17 13:45:37 +0300

    [Chore] merging items contollers to one

--- app/scripts/controllers/items.js
+++ app/scripts/controllers/items.js
@@ -1,12 +1,14 @@
 (function() {
   'use strict';
-
   angular.module('vvida.controllers')
-    .controller('ItemsCtrl', ['$timeout', '$scope', '$state', 'Categories', 'Items', 'Utils',
-      function($timeout, $scope, $state, Categories, Items, Utils) {
+    .controller('ItemCtrl', ['$scope', '$stateParams', 'Categories', 'FileUploader', 'Utils', 'Items',
+      function($scope, $stateParams, Categories, FileUploader, Utils, Items) {
+
+        // load categories
         $scope.loadCategories = function() {
           $scope.categories = Categories.query();
         };
+
         $scope.addItems = function() {
           Items.save($scope.item, function(item) {
             if (item) {
@@ -18,6 +20,45 @@
             }
           });
         };
+
+        var itemId = $stateParams.id;
+        var init = function() {
+          $scope.item = {
+            id: $stateParams.id
+          };
+          $scope.uploader = new FileUploader({
+            url: '/api/image/',
+            alias: 'photos',
+            formData: [$scope.item],
+          });
+        };
+
+        //load the item
+        Items.get({
+          id: itemId
+        }, function(item) {
+          $scope.images = item.Images;
+          console.log($scope.images);
+          $scope.item = item;
+        });
+
+        $scope.updateItem = function() {
+          Items.update($scope.item, function(item) {
+            Utils.toast(item.message);
+          });
+        };
+
+
+        $scope.showToast = function() {
+          Utils.toast('Upload complete');
+        };
+
+        $scope.upload = function() {
+          $scope.uploader.uploadAll();
+        };
+
+        init();
+        console.log($scope.uploader.progress);
       }
     ]);
 })();
