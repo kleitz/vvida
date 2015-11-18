e77cafd4a8431c079ba29fd66e566e232fbadfb0
andela-thomas <thomas.nyambati@andela.com>
2015-11-18 11:47:15 +0300

    [ Finishes #108305440] Add template text on the about page]

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,28 +1,39 @@
 angular.module('vvida.controllers')
-  .controller('AboutCtrl', ['$scope', 'Utils', function($scope, Utils) {
+  .controller('AboutCtrl', ['$scope', function($scope) {
     $scope.developers = [{
       name: 'Godson Ukpere',
       title: 'Chief Technical Officer',
       img_url: 'https://avatars1.githubusercontent.com/u/2857896?v=3&s=192'
     }, {
       name: 'Nyambati N Thomas',
-      title: 'Junior Consultant',
+      title: 'Back-End/API Engineer',
       img_url: 'https://avatars3.githubusercontent.com/u/12892110?v=3&s=460'
     }, {
       name: 'Eugene Mutai',
-      title: 'Junior Consultant',
+      title: 'Staff Developer Advocate',
       img_url: 'https://avatars2.githubusercontent.com/u/1999708?v=3&s=460'
     }, {
       name: 'Hannah Koske',
-      title: 'Junior Consultant',
+      title: 'Front-End Engineer',
       img_url: 'https://avatars3.githubusercontent.com/u/13534520?v=3&s=192'
     }, {
       name: 'Teddy Otieno Asola',
-      title: 'Junior Consultant',
+      title: 'UI/UX Designer',
       img_url: 'https://avatars3.githubusercontent.com/u/12892106?v=3&s=460'
     }, {
       name: 'Nate Martins Mwangi',
-      title: 'Junior Consultant',
+      title: 'Dev Ops',
       img_url: 'https://avatars1.githubusercontent.com/u/12892047?v=3&s=460'
     }];
+
+    $scope.about = {
+      img_url: '',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod' +
+        'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,' +
+        'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo' +
+        'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse' +
+        'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non' +
+        'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    };
+
   }]);
