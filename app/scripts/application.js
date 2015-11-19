cc5dfbe8b291b976548d4df3ea5297bdd9c6136e
Hannah Koske <hannah.koske@andela.com>
2015-11-19 09:47:40 +0300

    [#108515678] Replace BEND user session with locally stored user object

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -47,15 +47,7 @@
     function($rootScope, $location, $state, $mdSidenav, Users, Auth) {
       // Check if the user's session is still being persisted in the servers
       if (Auth.isLoggedIn()) {
-        Users.session(function(err, res) {
-          if (!err) {
-            $rootScope.currentUser = res;
-          } else {
-            console.log('Error: ', err.error);
-          }
-        });
-      } else {
-        $state.go('home');
+        $rootScope.currentUser = Auth.getUser();
       }
 
       $rootScope.login = function() {
@@ -89,103 +81,104 @@
   window.app.config(['$stateProvider', '$httpProvider', '$urlRouterProvider', '$locationProvider', '$mdThemingProvider',
     function($stateProvider, $httpProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider) {
 
-    $httpProvider.interceptors.push('TokenInjector');
+      $httpProvider.interceptors.push('TokenInjector');
 
-    // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise('/404');
+      // For any unmatched url, redirect to /state1
+      $urlRouterProvider.otherwise('/404');
 
 
-    // Now set up the states
-    $mdThemingProvider.theme('default')
-      .primaryPalette('blue')
-      .accentPalette('deep-orange');
+      // Now set up the states
+      $mdThemingProvider.theme('default')
+        .primaryPalette('blue')
+        .accentPalette('deep-orange');
 
-    $stateProvider
-      .state('home', {
-        url: '/',
-        controller: 'HomeCtrl',
-        templateUrl: 'views/home.html'
-      })
-      .state('about', {
-        url: '/about',
-        controller: 'AboutCtrl',
-        templateUrl: 'views/about.html'
-      })
-      .state('events', {
-        url: '/events',
-        controller: 'EventsCtrl',
-        templateUrl: 'views/events.html'
-      })
-      .state('profile', {
-        url: '/user/{id}/edit',
-        controller: 'ProfileCtrl',
-        templateUrl: 'views/edit-profile.html'
-      })
-      .state('userProfile', {
-        url: '/user/profile',
-        views: {
-          '': {
-            controller: 'UserProfileCtrl',
-            templateUrl: 'views/user-profile.html',
-          },
-          'Reviews@userProfile': {
-            controller: 'UserReviewsCtrl',
-            templateUrl: 'views/user-reviews.html',
-          },
-          'Events@userProfile': {
-            controller: 'UserEventsCtrl',
-            templateUrl: 'views/user-events.html',
-          },
-          'Products@userProfile': {
-            controller: 'UserProductsCtrl',
-            templateUrl: 'views/user-products.html',
-          },
-          'Pictures@userProfile': {
-            controller: 'UserPicturesCtrl',
-            templateUrl: 'views/user-pictures.html',
+      $stateProvider
+        .state('home', {
+          url: '/',
+          controller: 'HomeCtrl',
+          templateUrl: 'views/home.html'
+        })
+        .state('about', {
+          url: '/about',
+          controller: 'AboutCtrl',
+          templateUrl: 'views/about.html'
+        })
+        .state('events', {
+          url: '/events',
+          controller: 'EventsCtrl',
+          templateUrl: 'views/events.html'
+        })
+        .state('profile', {
+          url: '/user/{id}/edit',
+          controller: 'ProfileCtrl',
+          templateUrl: 'views/edit-profile.html'
+        })
+        .state('userProfile', {
+          url: '/user/profile',
+          views: {
+            '': {
+              controller: 'UserProfileCtrl',
+              templateUrl: 'views/user-profile.html',
+            },
+            'Reviews@userProfile': {
+              controller: 'UserReviewsCtrl',
+              templateUrl: 'views/user-reviews.html',
+            },
+            'Events@userProfile': {
+              controller: 'UserEventsCtrl',
+              templateUrl: 'views/user-events.html',
+            },
+            'Products@userProfile': {
+              controller: 'UserProductsCtrl',
+              templateUrl: 'views/user-products.html',
+            },
+            'Pictures@userProfile': {
+              controller: 'UserPicturesCtrl',
+              templateUrl: 'views/user-pictures.html',
+            }
           }
-        }
-      })
-      .state('addItem', {
-        url: '/items/create',
-        controller: 'ItemCtrl',
-        templateUrl: 'views/edit-item.html'
-      })
-      .state('editItem', {
-        url: '/items/{id}/edit',
-        controller: 'ItemCtrl',
-        templateUrl: 'views/edit-item.html'
-      })
-      .state('addEvent', {
-        url: '/events/create',
-        controller: 'EventCtrl',
-        templateUrl: 'views/add-event.html'
-      })
-      .state('editEvent', {
-        url: '/events/{id}/edit',
-        controller: 'EventCtrl',
-        templateUrl: 'views/edit-event.html'
-      })
-      .state('login', {
-        url: '/users/login',
-        controller: 'LoginCtrl',
-        templateUrl: 'views/login.html'
-      })
-      .state('welcome', {
-        url: '/welcome',
-        controller: 'WelcomeCtrl',
-        templateUrl: 'views/welcome.html'
-      })
-      .state('upload', {
-        url: '/upload',
-        controller: 'AboutCtrl',
-        templateUrl: 'views/upload.html'
-      })
-      .state('404', {
-        url: '/404',
-        templateUrl: 'views/404.html'
-      });
-    $locationProvider.html5Mode(true);
-  }]);
+        })
+        .state('addItem', {
+          url: '/items/create',
+          controller: 'ItemCtrl',
+          templateUrl: 'views/edit-item.html'
+        })
+        .state('editItem', {
+          url: '/items/{id}/edit',
+          controller: 'ItemCtrl',
+          templateUrl: 'views/edit-item.html'
+        })
+        .state('addEvent', {
+          url: '/events/create',
+          controller: 'EventCtrl',
+          templateUrl: 'views/add-event.html'
+        })
+        .state('editEvent', {
+          url: '/events/{id}/edit',
+          controller: 'EventCtrl',
+          templateUrl: 'views/edit-event.html'
+        })
+        .state('login', {
+          url: '/users/login',
+          controller: 'LoginCtrl',
+          templateUrl: 'views/login.html'
+        })
+        .state('welcome', {
+          url: '/welcome',
+          controller: 'WelcomeCtrl',
+          templateUrl: 'views/welcome.html'
+        })
+        .state('upload', {
+          url: '/upload',
+          controller: 'AboutCtrl',
+          templateUrl: 'views/upload.html'
+        })
+        .state('404', {
+          url: '/404',
+          templateUrl: 'views/404.html'
+        });
+      $locationProvider.html5Mode(true);
+    }
+  ]);
 
 })();
