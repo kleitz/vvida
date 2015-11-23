8651bc88d2b8092b819a9ccdac88aa85f19e669f
andela-nmartin <godmetweenciati@gmail.com>
2015-11-23 12:43:41 +0300

    [#108415446] Enables timestamps in items migration

--- migrations/create-items.js
+++ migrations/create-items.js
@@ -46,6 +46,8 @@
           // prevent time stamps from using camelase
           // updatedAt to updated_at and createdAt to created-at
           underscored: true,
+          // enabling timestamps
+          timestamps: true,
           // prevent sequelize from transforming the user tables to prural
           freezetableName: true
         });
