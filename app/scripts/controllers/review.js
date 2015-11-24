057711ba4b9b2bb7514d9e45aaf3d1611f2d823d
Teddy Otieno <teddy.asola@andela.com>
2015-11-24 17:13:51 +0300

    [#108415446] Add first name of current user logged in

--- app/scripts/controllers/review.js
+++ app/scripts/controllers/review.js
@@ -4,8 +4,10 @@
     .controller('ReviewCtrl', ['$scope', '$state', '$stateParams', 'Reviews', 'Categories', 'Utils',
       function($scope, $state, $stateParams, Categories, Utils, Reviews) {
         $scope.loadReviews = function() {
+          console.log($scope.currentUser);
           $scope.reviews = Reviews.query();
         };
+        $scope.user = $scope.currentUser.firstname;
         console.log($scope.reviews);
       }
       ]);
