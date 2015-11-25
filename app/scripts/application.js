e5c188b16a767fd158c3ce41f0f4233b131f6ce4
Hannah Koske <hannah.koske@andela.com>
2015-11-25 15:10:11 +0300

    [#107892180] Fix spacing and indentation

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -146,48 +146,49 @@
               controller: 'UserPicturesCtrl',
               templateUrl: 'views/user-pictures.html',
             }
-        }
-      })
-      .state('addItem', {
-        url: '/items/create',
-        controller: 'ItemCtrl',
-        templateUrl: 'views/items.html'
-      })
-      .state('editItem', {
-        url: '/items/{id}/edit',
-        controller: 'ItemCtrl',
-        templateUrl: 'views/edit-item.html'
-      })
-      .state('addEvent', {
-        url: '/events/create',
-        controller: 'EventCtrl',
-        templateUrl: 'views/add-event.html'
-      })
-      .state('editEvent', {
-        url: '/events/{id}/edit',
-        controller: 'EventCtrl',
-        templateUrl: 'views/edit-event.html'
-      })
-      .state('login', {
-        url: '/users/login',
-        controller: 'LoginCtrl',
-        templateUrl: 'views/login.html'
-      })
-      .state('welcome', {
-        url: '/welcome',
-        controller: 'WelcomeCtrl',
-        templateUrl: 'views/welcome.html'
-      })
-      .state('upload', {
-        url: '/upload',
-        controller: 'AboutCtrl',
-        templateUrl: 'views/upload.html'
-      })
-      .state('404', {
-        url: '/404',
-        templateUrl: 'views/404.html'
-      });
-    $locationProvider.html5Mode(true);
-  }]);
+          }
+        })
+        .state('addItem', {
+          url: '/items/create',
+          controller: 'ItemCtrl',
+          templateUrl: 'views/items.html'
+        })
+        .state('editItem', {
+          url: '/items/{id}/edit',
+          controller: 'ItemCtrl',
+          templateUrl: 'views/edit-item.html'
+        })
+        .state('addEvent', {
+          url: '/events/create',
+          controller: 'EventCtrl',
+          templateUrl: 'views/add-event.html'
+        })
+        .state('editEvent', {
+          url: '/events/{id}/edit',
+          controller: 'EventCtrl',
+          templateUrl: 'views/edit-event.html'
+        })
+        .state('login', {
+          url: '/users/login',
+          controller: 'LoginCtrl',
+          templateUrl: 'views/login.html'
+        })
+        .state('welcome', {
+          url: '/welcome',
+          controller: 'WelcomeCtrl',
+          templateUrl: 'views/welcome.html'
+        })
+        .state('upload', {
+          url: '/upload',
+          controller: 'AboutCtrl',
+          templateUrl: 'views/upload.html'
+        })
+        .state('404', {
+          url: '/404',
+          templateUrl: 'views/404.html'
+        });
+      $locationProvider.html5Mode(true);
+    }
+  ]);
 
 })();
