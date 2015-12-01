36c9d5d181ab5cc39c4af5ac519914341a6c2317
Hannah Koske <hannah.koske@andela.com>
2015-12-01 08:34:34 +0300

    [#108836586] Add database url to process.env

--- server/config/database.js
+++ server/config/database.js
@@ -9,7 +9,8 @@ var config = {
   database: process.env.DATABASE_NAME,
   host: process.env.DATABASE_HOST,
   dialect: process.env.DATABASE_DIALECT,
-  port: process.env.DATABASE_PORT
+  port: process.env.DATABASE_PORT,
+  url: process.env.DATABASE_URL
 };
 
 module.exports = {
