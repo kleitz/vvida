ba070111c5a8f960f74f68d8526ff1d7d93698ab
andela-nmartin <godmetweenciati@gmail.com>
2015-12-02 15:15:32 +0300

    [#103855550] Adds more tests for FEND services

---
+++ tests/unit/client/services/promotions.spec.js
@@ -0,0 +1,23 @@
+describe('Promotions Service Test', function() {
+
+  beforeEach(function() {
+    module('vvida');
+  });
+
+  var Promotions,
+  $resource;
+  beforeEach(inject(function($injector) {
+    Promotions = $injector.get('Promotions');
+    $resource = $injector.get('$resource');
+    spyOn(Promotions, "update").and.returnValue();
+    Promotions.update();
+  }));
+
+  describe('Promotions unit tests', function() {
+    it('update should be a function', function() {
+      expect(Promotions.update).toBeDefined();
+      expect(Promotions.update).toHaveBeenCalled();
+      expect(typeof Promotions.update).toBe('function');
+    });
+  });
+});
