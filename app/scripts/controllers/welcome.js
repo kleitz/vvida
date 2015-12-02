80496e26fedaa99610145c5e4be0d66d06913378
andela-thomas <thomas.nyambati@andela.com>
2015-12-02 16:15:23 +0300

    [chore]  Fix hounci issues

--- app/scripts/controllers/welcome.js
+++ app/scripts/controllers/welcome.js
@@ -7,16 +7,16 @@ angular.module('vvida.controllers')
     console.log($scope.items);
     $scope.item = {
       face: 'http://lorempixel.com/50/50/people?' + 9,
-      who: " write something about this company, and live the life you want",
-      what: "And experince the magic of you opinion",
-      notes: "Hey " + $rootScope.currentUser.name + " , have you made any review yet? your next review awaits, "
+      who: ' write something about this company, and live the life you want',
+      what: 'And experince the magic of you opinion',
+      notes: 'Hey ' + $rootScope.currentUser.name + ' , have you made any review yet? your next review awaits, '
     };
     for (var i = 0; i < 3; i++) {
       $scope.todos.push({
         face: 'http://lorempixel.com/100/100/people?' + i,
-        what: "Brunch this weekend?",
-        who: "Min Li Chan",
-        notes: "I'll be in your neighborhood doing errands."
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        notes: 'I\'ll be in your neighborhood doing errands.'
       });
     }
   }]);
--- server/routes/account.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app, auth) {
-  // login with email
-  app.route('/account')
-    .get(function(req, res) {
-      console.log({
-        user: req.user
-      });
-    });
-};
