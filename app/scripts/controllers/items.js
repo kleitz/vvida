014ac78513d00441c04b88df0f1787a60a18c4e8
andela-blawrence <lawrence.olaiya@andela.com>
2015-12-22 19:29:12 +0100

    [Fixes #107892466] move assignation of state parameter(item id) to the init function. Should not be global.

--- app/scripts/controllers/items.js
+++ app/scripts/controllers/items.js
@@ -3,8 +3,7 @@
   angular.module('vvida.controllers')
     .controller('ItemCtrl', ['$scope', '$state', '$stateParams', 'Categories', 'FileUploader', 'Utils', 'Items',
       function($scope, $state, $stateParams, Categories, FileUploader, Utils, Items) {
-
-        // load categories
+        // Load categories
         $scope.loadCategories = function() {
           $scope.categories = Categories.query();
         };
@@ -22,18 +21,19 @@
           });
         };
 
-        var itemId = $stateParams.id;
         var init = function() {
+          var itemId = $stateParams.id;
           $scope.item = {
             id: $stateParams.id
           };
+
           $scope.uploader = new FileUploader({
             url: '/api/image/',
             alias: 'photos',
             formData: [$scope.item],
           });
         };
-        //load the item
+        // Load the item
         if (itemId) {
           Items.get({
             id: itemId
@@ -42,12 +42,12 @@
             $scope.item = item;
           });
         }
+
         $scope.updateItem = function() {
           Items.update($scope.item, function(item) {
             Utils.toast(item.message);
           });
         };
-
 
         $scope.showToast = function() {
           Utils.toast('Upload complete');
