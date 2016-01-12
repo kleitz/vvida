459de8134311f306d5d45dbaad51cbd7c83a1b29
Hannah Koske <hannah.koske@andela.com>
2016-01-12 17:04:48 +0300

    [#103855550] Add unit tests for Token Service methods called in the various Auth service  methods

--- tests/unit/client/services/auth.spec.js
+++ tests/unit/client/services/auth.spec.js
@@ -17,14 +17,38 @@ describe('Auth Service Test', function() {
       expect(typeof Auth.isLoggedIn).toBe('function');
     });
 
+    it('isLoggedIn should call Token.get and return true', function() {
+      Token.get = sinon.stub().returns(true);
+      expect(Auth.isLoggedIn()).toBe(true);
+      expect(Token.get.called).toBe(true);
+    });
+
+    it('isLoggedIn should return false if Token.get returns false', function() {
+      Token.get = sinon.stub().returns(false);
+      expect(Auth.isLoggedIn()).toBe(false);
+      expect(Token.get.called).toBe(true);
+    });
+
     it('setToken should be a function', function() {
       expect(Auth.setToken).toBeDefined();
       expect(typeof Auth.setToken).toBe('function');
     });
 
+    it('setToken should call Token.set', function() {
+      Token.set = sinon.spy();
+      Auth.setToken('vvida');
+      expect(Token.set.called).toBe(true);
+    });
+
     it('logout should be a function and be defined', function() {
       expect(Auth.logout).toBeDefined();
       expect(typeof Auth.logout).toBe('function');
+    });
+
+    it('logout should call Token.remove', function() {
+      Token.remove = sinon.spy();
+      Auth.logout();
+      expect(Token.remove.called).toBe(true);
     });
   });
 });
