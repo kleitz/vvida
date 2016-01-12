1111bb5d28e1c4682fca2f0e55dc4b44c7c207b2
Hannah Koske <hannah.koske@andela.com>
2016-01-12 17:10:21 +0300

    [#103855550] Add unit tests for $window.localStorage  methods called in the various Token methods

--- tests/unit/client/services/token.spec.js
+++ tests/unit/client/services/token.spec.js
@@ -5,10 +5,11 @@ describe('Token Service Test', function() {
   });
 
   var Token,
-    $window;
+    $window, localStorage;
   beforeEach(inject(function($injector) {
     Token = $injector.get('Token');
     $window = $injector.get('$window');
+    localStorage = $window.localStorage;
   }));
 
   describe('Token unit tests', function() {
@@ -17,14 +18,32 @@ describe('Token Service Test', function() {
       expect(typeof Token.set).toBe('function');
     });
 
+     it('set should call $window.localStorage.setItem', function() {
+      localStorage.setItem = sinon.stub();
+      Token.set('vvida');
+      expect(localStorage.setItem.called).toBe(true);
+    });
+
     it('get should be a function', function() {
       expect(Token.get).toBeDefined();
       expect(typeof Token.get).toBe('function');
     });
 
+    it('get should call $window.localStorage.getItem', function() {
+      localStorage.getItem = sinon.stub().returns(true);
+      expect(Token.get()).toBe(true);
+      expect(localStorage.getItem.called).toBe(true);
+    });
+
     it('remove should be a function and be defined', function() {
       expect(Token.remove).toBeDefined();
       expect(typeof Token.remove).toBe('function');
+    });
+
+    it('remove should call $window.localStorage.removeItem', function() {
+      localStorage.removeItem = sinon.stub();
+      Token.remove();
+      expect(localStorage.removeItem.called).toBe(true);
     });
   });
 });
