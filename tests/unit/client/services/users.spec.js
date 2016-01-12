32bed3a88716fc7fea568451e4dbb2570a304519
Hannah Koske <hannah.koske@andela.com>
2016-01-12 17:11:51 +0300

    [#103855550] Add unit tests for $http, $http.success and $http.error methods called in the various Users Service  methods

--- tests/unit/client/services/users.spec.js
+++ tests/unit/client/services/users.spec.js
@@ -4,10 +4,19 @@ describe('Users Service Test', function() {
     module('vvida');
   });
 
-  var Users, $http;
+  var Users, $http, $httpBackend;
   beforeEach(inject(function($injector) {
-    Users = $injector.get('Users');
+    $httpBackend = $injector.get('$httpBackend');
+    $httpBackend.when('GET', '/api/users/session')
+      .respond(200, [{
+        res: 'res'
+      }]);
+    $httpBackend.when('GET', 'views/home.html')
+      .respond(200, [{
+        res: 'res'
+      }]);
     $http = $injector.get('$http');
+    Users = $injector.get('Users');
   }));
 
   describe('Users unit tests', function() {
@@ -16,9 +25,286 @@ describe('Users Service Test', function() {
       expect(typeof Users.login).toBe('function');
     });
 
-    it('$http and its method post should be defined', function() {
-      expect($http.post).toBeDefined();
-      expect(typeof $http.post).toBe('function');
+    it('login should call $http.success on status 200', function() {
+      $httpBackend.when('POST', '/api/users/login')
+        .respond(200, {
+          res: 'res'
+        });
+      var cb = sinon.spy();
+      Users.login({
+        user: 'user'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0]).toBe(null);
+      expect(cb.args[0][1].res).toBe('res');
     });
+
+    it('login should call $http.error on status 500', function() {
+      $httpBackend.when('POST', '/api/users/login')
+        .respond(500, {
+          err: 'err'
+        });
+      var cb = sinon.spy();
+      Users.login({
+        user: 'user'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0].err).toBe('err');
+    });
+
+    it('session should be a function', function() {
+      expect(Users.session).toBeDefined();
+      expect(typeof Users.session).toBe('function');
+    });
+
+    it('logout should be a function', function() {
+      expect(Users.logout).toBeDefined();
+      expect(typeof Users.logout).toBe('function');
+    });
+
+    it('logout should call $http.success on status 200', function() {
+      $httpBackend.when('GET', '/api/users/logout')
+        .respond(200, {
+          res: 'res'
+        });
+      var cb = sinon.spy();
+      Users.logout(cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0]).toBe(null);
+      expect(cb.args[0][1].res).toBe('res');
+    });
+
+    it('logout should call $http.error on status 500', function() {
+      $httpBackend.when('GET', '/api/users/logout')
+        .respond(500, {
+          err: 'err'
+        });
+      var cb = sinon.spy();
+      Users.logout(cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0].err).toBe('err');
+    });
+
+    it('items should be a function', function() {
+      expect(Users.items).toBeDefined();
+      expect(typeof Users.items).toBe('function');
+    });
+
+    it('items should call $http.success on status 200', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/items/,
+          undefined, undefined, ['id'])
+        .respond(200, {
+          res: 'res'
+        });
+      var cb = sinon.spy();
+      Users.items({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0]).toBe(null);
+      expect(cb.args[0][1].res).toBe('res');
+    });
+
+    it('items should call $http.error on status 500', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/items/,
+          undefined, undefined, ['id'])
+        .respond(500, {
+          err: 'err'
+        });
+      var cb = sinon.spy();
+      Users.items({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0].err).toBe('err');
+    });
+
+    it('itemsCount should be a function', function() {
+      expect(Users.itemsCount).toBeDefined();
+      expect(typeof Users.itemsCount).toBe('function');
+    });
+
+    it('itemsCount should call $http.success on status 200', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/items\/no/,
+          undefined, undefined, ['id'])
+        .respond(200, {
+          res: 'res'
+        });
+      var cb = sinon.spy();
+      Users.itemsCount({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0]).toBe(null);
+      expect(cb.args[0][1].res).toBe('res');
+    });
+
+    it('itemsCount should call $http.error on status 500', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/items\/no/,
+          undefined, undefined, ['id'])
+        .respond(500, {
+          err: 'err'
+        });
+      var cb = sinon.spy();
+      Users.itemsCount({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0].err).toBe('err');
+    });
+
+    it('reviews should be a function', function() {
+      expect(Users.reviews).toBeDefined();
+      expect(typeof Users.reviews).toBe('function');
+    });
+
+    it('reviews should call $http.success on status 200', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/reviews/,
+          undefined, undefined, ['id'])
+        .respond(200, {
+          res: 'res'
+        });
+      var cb = sinon.spy();
+      Users.reviews({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0]).toBe(null);
+      expect(cb.args[0][1].res).toBe('res');
+    });
+
+    it('reviews should call $http.error on status 500', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/reviews/,
+          undefined, undefined, ['id'])
+        .respond(500, {
+          err: 'err'
+        });
+      var cb = sinon.spy();
+      Users.reviews({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0].err).toBe('err');
+    });
+
+    it('reviewsCount should be a function', function() {
+      expect(Users.reviewsCount).toBeDefined();
+      expect(typeof Users.reviewsCount).toBe('function');
+    });
+
+    it('reviewsCount should call $http.success on status 200', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/reviews\/no/,
+          undefined, undefined, ['id'])
+        .respond(200, {
+          res: 'res'
+        });
+      var cb = sinon.spy();
+      Users.reviewsCount({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0]).toBe(null);
+      expect(cb.args[0][1].res).toBe('res');
+    });
+
+    it('reviewsCount should call $http.error on status 500', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/reviews\/no/,
+          undefined, undefined, ['id'])
+        .respond(500, {
+          err: 'err'
+        });
+      var cb = sinon.spy();
+      Users.reviewsCount({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0].err).toBe('err');
+    });
+
+    it('events should be a function', function() {
+      expect(Users.events).toBeDefined();
+      expect(typeof Users.events).toBe('function');
+    });
+
+    it('events should call $http.success on status 200', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/events/,
+          undefined, undefined, ['id'])
+        .respond(200, {
+          res: 'res'
+        });
+      var cb = sinon.spy();
+      Users.events({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0]).toBe(null);
+      expect(cb.args[0][1].res).toBe('res');
+    });
+
+    it('events should call $http.error on status 500', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/events/,
+          undefined, undefined, ['id'])
+        .respond(500, {
+          err: 'err'
+        });
+      var cb = sinon.spy();
+      Users.events({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0].err).toBe('err');
+    });
+
+    it('eventsCount should be a function', function() {
+      expect(Users.eventsCount).toBeDefined();
+      expect(typeof Users.eventsCount).toBe('function');
+    });
+
+    it('eventsCount should call $http.success on status 200', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/events\/no/,
+          undefined, undefined, ['id'])
+        .respond(200, {
+          res: 'res'
+        });
+      var cb = sinon.spy();
+      Users.eventsCount({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0]).toBe(null);
+      expect(cb.args[0][1].res).toBe('res');
+    });
+
+    it('eventsCount should call $http.error on status 500', function() {
+      $httpBackend.whenGET(/\/api\/users\/(.+)\/events\/no/,
+          undefined, undefined, ['id'])
+        .respond(500, {
+          err: 'err'
+        });
+      var cb = sinon.spy();
+      Users.eventsCount({
+        id: 'id'
+      }, cb);
+      $httpBackend.flush();
+      expect(cb.called).toBe(true);
+      expect(cb.args[0][0].err).toBe('err');
+    });
+
+
   });
 });
