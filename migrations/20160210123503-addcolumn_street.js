4b734e31fac2c0bc33be4259f1216f6335384554
andela-eakinyele <emmanuel.akinyele@andela.com>
2016-02-10 15:01:40 +0100

    Alter migrations scripts for application
    [Fixes #113071249]
    
    - Create migrations for altering tables
    - Specify new path for build migrations

---
+++ migrations/20160210123503-addcolumn_street.js
@@ -0,0 +1,17 @@
+(function() {
+
+  'use strict';
+
+  module.exports = {
+    up: function(queryInterface, Sequelize) {
+      return queryInterface.addColumn('Items', 'street', {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+    },
+
+    down: function(queryInterface, Sequelize) {
+
+    }
+  };
+})();
---
+++ migrations/20160210123617-addcolumn_email.js
@@ -0,0 +1,17 @@
+(function() {
+
+  'use strict';
+
+  module.exports = {
+    up: function(queryInterface, Sequelize) {
+      return queryInterface.addColumn('Items', 'email', {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+    },
+
+    down: function(queryInterface, Sequelize) {
+
+    }
+  };
+})();
