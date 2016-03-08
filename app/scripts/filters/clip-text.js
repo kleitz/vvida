a7088684f53b84fb14534d5dc3a5596bdb707188
andela-eakinyele <emmanuel.akinyele@andela.com>
2016-03-08 17:55:23 +0100

    Redesign HomePage
    Refers to [#113999041]
    
    - Reduce height of jumbotron
    - Replace divs with md-cards
    - Use filter for truncating text

---
+++ app/scripts/filters/clip-text.js
@@ -0,0 +1,18 @@
+angular.module('vvida.filters')
+  .filter('ngClipText', function() {
+    return function(input, arg) {
+      if (input.length > arg) {
+        var str = input.substring(arg, 1);
+        if (str === ' ') {
+          return input.substring(0, arg) + '...';
+        }
+        var end = input.indexOf(' ', arg - 1)
+        if (end === -1) {
+          end = input.length;
+        }
+        return input.substring(0, end) + '...';
+      } else {
+        return input;
+      }
+    }
+  });
